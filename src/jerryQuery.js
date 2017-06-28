f1a03f39fdcc41f29643b8201872ec8d08c74fbe
szy0syz <szy0syz@gmail.com>
2017-06-28 16:25:07 +0800

    模拟jQuery-Selecotor~好难~
    真的好难，不知道怎写。再看看。

--- src/jerryQuery.js
+++ src/jerryQuery.js
@@ -1,13 +1,30 @@
 ~function () {
-    // 构造函数
-    var jQuery = function (selector, context) {
-        return new jQuery.fn.init(selector, context)
+  // 构造函数
+  var jQuery = function (selector, context) {
+    return new jQuery.fn.init(selector, context)
+  };
+  // 修改原型
+  jQuery.fn = jQuery.prototype = {
+    constructor: jQuery,
+    init: Init
+  };
+  
+  function Init (selector, context) {
+    this.selector = selector;
+    this.content = context || document;
+    //情况1：selector为字符串，启动选择器默认。
+    if (typeof selector === "string") {
+      //状态1:仅仅为一个*号时
+      if (selector === "*") {
+        console.log("你输入的是*");
+      } else if (selector) {
+      
+      }
+      this[0] = 'html';
+      this[1] = 'head';
+      this.length = 2;
     }
-    // 修改原型
-    jQuery.fn = jQuery.prototype = {
-        constructor: jQuery,
-        init: function (selector, context) {...}
-    }
-    // 设置全局变量
-    window.jQuery = window.$ = jQuery;
-} ();
+  }
+  // 设置全局变量
+  window.jQuery = window.$$ = jQuery;
+}();
